bbac513eb836e2b411a96fae220ddd09e2d1809d
An Le Dai <ledaian22@gmail.com>
2025-10-15 04:20:15 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
-
 import ExploreContainer from "@/modules/explore/explore-container";
-import { getQueryClient } from "@/providers/get-query-client";
-import { galleryInfiniteOptions } from "@/routers/gallery";
 
 export default function Home() {
-  const queryClient = getQueryClient();
-
-  void queryClient.prefetchInfiniteQuery(galleryInfiniteOptions());
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <ExploreContainer />
-    </HydrationBoundary>
-  );
+  return <ExploreContainer />;
 }
